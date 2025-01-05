362f19b360351b6bc024dd61aa33a6b984f7be57
thapasijan17 <thapasijan329@gmail.com>
2025-01-05 13:14:02 +0545

    feat(nvim): start code for any user

---
+++ init.lua
@@ -0,0 +1,12 @@
+vim.cmd.colorscheme("nirvana")
+
+require("impatient")
+-- All The Plugins
+require("plugins")
+
+-- Core Configuration
+require("core.options")
+require("core.keybinds")
+
+-- Loading The Statusline
+require("staline")
