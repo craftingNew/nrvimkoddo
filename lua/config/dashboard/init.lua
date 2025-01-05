78ff08b7e2b522595a20d0a1cfd368d93fb364c0
thapasijan17 <thapasijan329@gmail.com>
2025-01-05 18:46:17 +0545

    chore(colorscheme): add telescope alpha colorscheme changes

---
+++ lua/config/dashboard/init.lua
@@ -0,0 +1,36 @@
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+math.randomseed(os.time())
+
+local logo = {
+    "                                   ",
+    "                                   ",
+    "                                   ",
+    "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
+    "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+    "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
+    "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+    "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+    "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+    "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+    " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+    " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
+    "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+    "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+    "                                   ",
+}
+
+dashboard.section.header.val = logo
+dashboard.section.header.opts.hl = "AlphaHeader"
+dashboard.section.buttons.opts.hl = "AlphaButtons"
+dashboard.section.buttons.val = {
+  dashboard.button("Ctrl + B", "  File Explorer", ":NvimTreeToggle<cr>"),
+  dashboard.button("Leader + F", "  Find File", ":Telescope find_files <cr>"),
+  dashboard.button("Leader + R", "  Find Word", ":Telescope live_grep<cr>"),
+  dashboard.button("Leader + G", "  Recently Opened", ":Telescope oldfiles<cr>"),
+  dashboard.button("q", "  Quit", ":qa<cr>")
+}
+
+alpha.setup(dashboard.opts)
+vim.cmd([[ autocmd FileType alpha setlocal nofoldenable]])
+
--- lua/themes/init.lua
+++
@@ -1,9 +0,0 @@
-local highlights = require('themes.highlights')
-
-local function setup(opts)
-  if opts == nil then opts = { theme = "nirvana" } end
-  local colors = require('themes.colorschemes.' .. opts.theme).get_colors()
-  highlights.highlight_all(colors, opts)
-end
-
-return { setup = setup }
